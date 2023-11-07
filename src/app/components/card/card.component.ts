884d485ca1ada99cfd711ba5defcb88f2e597788
ArthurAthayde <arthur.athaydee@gmail.com>
2023-11-06 21:25:57 -0300

    feat: expand card options

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -7,7 +7,27 @@ import { MatCardModule } from '@angular/material/card';
   styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-  laisDescription = `Psicóloga com título de especialista em Psicologia Clínica e Psicologia da Saúde pelo Conselho Federal de Psicologia. Especialista em Saúde Mental e Psiquiatria pelo Hospital das Clínicas da USP. Formação em Psicopatologia.
+  sliceOptions = {
+    start: 0,
+    end: 100 as number | undefined,
+    default: 100,
+  };
 
-  Psicóloga no SUS desde 2016 em serviços da rede de atenção psicossocial.`;
+  expandText = 'Ver mais';
+
+  details = {
+    desc: `Psicóloga com título de especialista em Psicologia Clínica e Psicologia da Saúde pelo Conselho Federal de Psicologia. Especialista em Saúde Mental e Psiquiatria pelo Hospital das Clínicas da USP. Formação em Psicopatologia.
+
+  Psicóloga no SUS desde 2016 em serviços da rede de atenção psicossocial.`,
+  };
+
+  onExpandText(evt: any): void {
+    this.sliceOptions.end =
+      this.sliceOptions.end === this.sliceOptions.default
+        ? undefined
+        : this.sliceOptions.default;
+
+    this.expandText =
+      this.expandText === this.expandText ? 'Ver menos' : 'Ver mais';
+  }
 }
